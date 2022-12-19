d5c7bf04884dec482e9500173f6f72659b4014f8
lyl <1552412358@qq.com>
2022-12-19 21:48:52 +0800

    使用vue3脚手架搭建页面基本结构

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        新闻发布管理系统 ©2022 Created by lyl
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        name: 'the-footer',
+    });
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/src/components/the-header.vue
@@ -0,0 +1,30 @@
+<template>
+    <a-layout-header class="header">
+        <div class="logo" />
+        <a-menu
+                v-model:selectedKeys="selectedKeys1"
+                theme="dark"
+                mode="horizontal"
+                :style="{ lineHeight: '64px' }"
+        >
+            <a-menu-item key="/">
+                <router-link to="/">首页</router-link>
+            </a-menu-item>
+            <a-menu-item key="/admin/newsList">
+                <router-link to="/admin/newsList">新闻管理</router-link>
+            </a-menu-item>
+            <a-menu-item key="3">nav 3</a-menu-item>
+        </a-menu>
+    </a-layout-header>
+</template>
+
+<script language="ts">
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        name: 'the-header',
+    });
+</script>
+
+<style scoped>
+
+</style>
